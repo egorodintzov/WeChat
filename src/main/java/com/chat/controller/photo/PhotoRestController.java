ad08d9a201b25ded2757d66b2222c1a0c197c2b7
Opachi <egorodinczov@yandex.ru>
2020-09-18 19:18:29 +0300

    issue(namespaces url)

--- src/main/java/com/chat/controller/photo/PhotoRestController.java
+++ src/main/java/com/chat/controller/photo/PhotoRestController.java
@@ -11,7 +11,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 
 @RestController
-@RequestMapping("/api/p")
+@RequestMapping("/api")
 public class PhotoRestController {
 
      @Autowired
